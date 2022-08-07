d94785086c715f8c8c290ead5bffea59a766d457
dav033 <87620700+dav033@users.noreply.github.com>
2022-08-07 11:00:17 -0500

    a

--- dist/private-chat/private-chat.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createPrivateChatDto = void 0;
-class createPrivateChatDto {
-}
-exports.createPrivateChatDto = createPrivateChatDto;
-//# sourceMappingURL=private-chat.dto.js.map

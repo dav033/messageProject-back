47489b8fd3333f252374937e8ece8d9850e4a04f
dav033 <87620700+dav033@users.noreply.github.com>
2022-08-07 14:11:56 -0500

    a

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+require('dotenv').config()
+
 const cors = require('cors')
 async function bootstrap () {
   const app = await NestFactory.create(AppModule, { cors: true })
@@ -13,8 +15,6 @@ async function bootstrap () {
     })
   )
 
-  await app.listen(5000, () => {
-    console.log('server listening in port 5000')
-  })
+  await app.listen(process.env.PORT || 5000)
 }
 bootstrap()

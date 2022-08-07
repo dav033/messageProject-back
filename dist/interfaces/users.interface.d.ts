d552cc7052ae6dd41c29889037c7faae398232cc
dav033 <87620700+dav033@users.noreply.github.com>
2022-08-07 15:16:30 -0500

    u

--- dist/interfaces/users.interface.d.ts
+++
@@ -1,9 +0,0 @@
-import { Document } from 'mongoose';
-export interface Users extends Document {
-    userName: string;
-    email: string;
-    password: string;
-    rooms: string[];
-    privateChats: string[];
-    profileImage?: string;
-}

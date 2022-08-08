e0c17dd80dfd2c72db2b1c24e057e427c4b43800
dav033 <87620700+dav033@users.noreply.github.com>
2022-08-07 20:11:15 -0500

    a

--- src/events.gateway.ts
+++ src/events.gateway.ts
@@ -1,5 +1,4 @@
 import {
-  MessageBody,
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
@@ -8,15 +7,91 @@ import {
   WebSocketServer
 } from '@nestjs/websockets'
 import { Server, Socket } from 'socket.io'
-const usersList = []
+let usersList = []
 
-@WebSocketGateway()
-export class ChatGateway {
-  @WebSocketServer()
-    server
+@WebSocketGateway(80, {
+  cors: true
+})
+export class EventsGateway
+implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect {
+  @WebSocketServer() server: Server
 
-  @SubscribeMessage('message')
-  handleMessage (@MessageBody() message: string): void {
-    this.server.emit('message', message)
+  afterInit (server: any) {
+    console.log('Esto se ejecuta cuando inicia')
+  }
+
+  handleConnection (client: any, id) {
+    console.log('Hola alguien se conecto al socket 👌👌👌')
+  }
+
+  handleDisconnect (client: any) {
+    console.log('ALguien se fue! chao chao')
+    usersList = usersList.filter((user) => user.socket_id !== client.id)
+    this.server.emit('usersList', usersList)
+  }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom (client: Socket, room: string) {
+    client.join(room)
+    console.log('usuario conectado a la sala ', room)
+  }
+
+  @SubscribeMessage('conectado')
+  handleUserConnection (client: Socket, id: string) {
+    console.log(client.id, id)
+    this.server.to(client.id).emit('identifier', client.id)
+    usersList.push({ user_id: id, socket_id: client.id })
+    console.log(usersList)
+    this.server.emit('usersList', usersList)
+  }
+
+  @SubscribeMessage('refreshReceiver')
+  handleRefresh (client: Socket, receiverS: string) {
+    console.log('ppwPWPWPWPP', receiverS)
+    this.server.to(receiverS).emit('refresh')
+  }
+
+  @SubscribeMessage('sendMessageToRoom')
+  handleSendMessage (
+    client: Socket,
+    data: {
+      type: string
+      content: string
+      transmitter: string
+      context: string
+      room: string
+      createdAt: string
+    }
+  ) {
+    // console.log('ads', data)
+    const { content, transmitter, context, room, createdAt } = data
+
+    this.server.to(room).emit('message', {
+      content,
+      transmitter,
+      context,
+      room,
+      createdAt
+    })
+  }
+
+  @SubscribeMessage('sendMessageToprivateChat')
+  handleSendMessageToPrivateCahat (data: {
+    type: string
+    content: string
+    transmitter: string
+    context: string
+    room: string
+    createdAt: string
+  }) {
+    const { content, transmitter, context, room, createdAt } = data
+
+    this.server.to(room).emit('privateMessage', {
+      content,
+      transmitter,
+      context,
+      room,
+      createdAt
+    })
   }
 }
